fed657fd4e6d5fbb22d5e70c102f2e0670e5efc2
Johannes Katzer <johannes.katzer@maximago.de>
2020-12-08 18:57:37 +0100

    day 6 part 2

--- d6_sol.py
+++ d6_sol.py
@@ -28,3 +28,34 @@ def solve(path: str):
     return res
 
 solve('d6_inpt.txt')
+
+def readInputPart2(path: str):
+    f = open(path)
+    groupAnswers = []
+    groupAnswer = []
+    for line in f.readlines():
+        if (line == "\n"):
+            groupAnswers.append(groupAnswer)
+            groupAnswer = []
+            continue
+        groupAnswer.append(line.replace("\n", ""))
+    else:
+        groupAnswers.append(groupAnswer)
+
+    f.close()
+    return groupAnswers
+
+def solvePart2(path: str):
+    answers = readInputPart2(path)
+    res = 0
+    for answerList in answers:
+        matches = set(answerList.pop(0))
+        for answer in answerList:
+            matches &= set(answer)
+        count = len(matches)
+        # print(count)
+        res += count
+    print(res)
+    return res
+
+solvePart2('d6_inpt.txt')
